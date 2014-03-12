efa77c513c40745d71bfe22cf5d2191c482c4fc5
Willian Junior <guns945@gmail.com>
2014-03-12 20:14:55 +0000

    report first draft done

--- src/main.erl
+++ src/main.erl
@@ -6,11 +6,11 @@ start() ->
 	display:start(),
 	tempConv:start(),
 	clock:start(),
-	sensor:start(celsiusSensor, celsius),
-	sensor:start(fahrenheitSensor, fahrenheit),
+	sensor:start(celsiusSensor, fahrenheit2celsius),
+	sensor:start(fahrenheitSensor, celsius2fahrenheit),
 
 	% add the functions and sensors
-	tempConv:add_new_fun(celsius, fun(X) -> X+1 end),
-	tempConv:add_new_fun(fahrenheit, fun(X) -> X*2 end),
+	tempConv:add_new_fun(fahrenheit2celsius, fun(X) -> (X-32)*5/9 end),
+	tempConv:add_new_fun(celsius2fahrenheit, fun(X) -> (X*9/5)+32 end),
 	clock:add_new_sensor(celsiusSensor),
 	clock:add_new_sensor(fahrenheitSensor).
